0b9d4e43cb735116d10a7338246dd2b0419a830d
Maryna Rastargueva <rastargueva.m@gmail.com>
2023-01-31 01:54:39 +0100

    fix: delete type and interface

--- homework4/src/models/group.model.ts
+++ homework4/src/models/group.model.ts
@@ -1,19 +1,5 @@
-// import joi from 'joi';
 import { Model, DataTypes } from 'sequelize';
 import sequelize from '../data-access/database';
-
-// type PERMISSION = 'READ' | 'WRITE' | 'DELETE' | 'SHARE' | 'UPLOAD_FILES';
-//
-// export const GroupSchema = joi.object({
-//     name: joi.string().required(),
-//     permissions: joi.array().items(joi.string()).required(),
-// });
-//
-// export interface GroupInterface {
-//     id: string;
-//     name: string;
-//     permissions: PERMISSION[];
-// }
 
 export class Group extends Model {}
 Group.init(
